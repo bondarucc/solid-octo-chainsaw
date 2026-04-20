e316ebb333544e87f59078d69aac1b2a9693aa97
Sergei Bondaruk <bondarucc@gmail.com>
2026-04-21 01:50:14 +0300

    seed fix

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,11 +1,8 @@
 import "./initDB.js"
 import { app } from "./configApp.js";
-import seed from "../prisma/seed.js";
 
-seed().then(() => {
-  app.listen(3535, () => {
-    console.log("Listening");
-  })
+app.listen(3535, () => {
+  console.log("Listening");
 })
 
 // app.listen(3535, () => {
