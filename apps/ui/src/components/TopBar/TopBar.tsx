93c4f5c293877a49f343c982460408bae1d354e7
Sergei Bondaruk <bondarucc@gmail.com>
2026-05-05 20:14:44 +0200

    admin nav bar

--- apps/ui/src/components/TopBar/TopBar.tsx
+++ apps/ui/src/components/TopBar/TopBar.tsx
@@ -1,24 +1,33 @@
 import { UserOutlined } from "@ant-design/icons"
 import { Avatar, Button, Divider, Flex, Typography } from "antd"
-import { useCallback } from "react"
+import { lazy, useCallback } from "react"
 import { useRevalidator } from "react-router"
-import { logout } from "../api/api"
-import useUserData from "../hooks/useUserData"
+import { logout } from "../../api/api"
+import useUserData from "../../hooks/useUserData"
+
+const LazyAdminNavBar = lazy(() => import("./AdminNavBar"))
 
 function TopBar() {
-  const {revalidate} = useRevalidator();
+  const { revalidate } = useRevalidator();
   const logoutOnClick = useCallback(async () => {
     await logout()
     // navigate("/login")
     revalidate()
   }, [revalidate])
 
-  const {userData} = useUserData()
+  const { userData } = useUserData()
 
   return (
     <div style={{ backgroundColor: "rgb(248, 236, 222)", height: "50px", paddingInline: "12px" }}>
-      <Flex justify="end" style={{ height: "100%" }} align="center">
-        <Avatar icon={<UserOutlined />} size="small" style={{ marginLeft: "auto" }}/>
+      <Flex style={{ height: "100%" }} align="center">
+
+        <div style={{ flexGrow: 1 }}>
+          {
+            userData?.role === "ADMIN" && <LazyAdminNavBar />
+          }
+        </div>
+
+        <Avatar icon={<UserOutlined />} size="small" />
         <Typography.Text style={{ marginLeft: "4px" }}>
           {userData?.login}
         </Typography.Text>
