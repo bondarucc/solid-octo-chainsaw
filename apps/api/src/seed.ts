a53c2b607266152ac90c1684f29b8e65c1a37633
Sergei Bondaruk <bondarucc@gmail.com>
2026-09-09 18:21:37 +0300

    seed

---
+++ apps/api/src/seed.ts
@@ -0,0 +1,28 @@
+import { hashSync } from "bcrypt";
+import { prisma } from "./initDB.js";
+async function seed() {
+  const login = process.env.ADM_LOGIN;
+  const pwd = process.env.ADM_PWD;
+  if (!login || !pwd)
+    throw "NO ADM SETUP";
+  await prisma.user.create({
+    data: {
+      role: "ADMIN",
+      login,
+      pwd: hashSync(pwd, 10),
+      createdAt: new Date(),
+    }
+  })
+}
+
+seed()
+  .then(() => {
+    console.log("SEEDED")
+  })
+  .catch(e => {
+    console.log(e);
+  })
+  .finally(() => {
+    prisma.$disconnect()
+  })
+
